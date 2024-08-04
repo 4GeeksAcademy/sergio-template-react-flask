7dff483c6564bf64410858aa79dd08421976b528
Sergio Arias Fernández <166837201+mwolft@users.noreply.github.com>
2024-08-04 08:55:38 +0000

    JWTBACK

--- src/api/models.py
+++ src/api/models.py
@@ -1,93 +1,123 @@
-from flask_sqlalchemy import SQLAlchemy
+"""
+Models.py define las tablas de la base de datos utilizando SQLAlchemy, que es un ORM (Object Relational Mapper).
+Define cómo se almacenan y relacionan los datos en la base de datos.
 
+El parámetro unique determina si los valores de una columna deben ser únicos en toda la tabla. 
+Esto significa que no puede haber dos filas en la tabla con el mismo valor en esta columna.
+• Objetivo: Garantizar la unicidad de los datos en la columna especificada.
+• Valor por defecto: False.
+
+El parámetro nullable determina si una columna puede contener valores NULL. 
+En otras palabras, especifica si una columna es obligatoria o no.
+• Objetivo: Indicar si los datos en la columna son opcionales (nullable=True) o obligatorios (nullable=False).
+• Valor por defecto: True.
+
+La función db.relationship se usa en SQLAlchemy para establecer relaciones entre tablas en la base de datos. 
+
+backref: Crea una propiedad en la tabla que permite acceder a todos, por ejemplo los posts asociados con un usuario con user.posts. 
+Este atributo es una lista de todas las instancias de Post relacionadas con ese usuario.
+
+lazy: Este es un parámetro que afecta cómo se cargan los datos relacionados.
+SQLAlchemy cargará los datos relacionados de la base de datos cuando se acceda a ellos por primera vez (carga "perezosa")
+
+La función __repr__ define una representación "oficial" de un objeto.
+Recibe un argumento self, que es una referencia a la instancia actual del objeto.
+
+Serialize convierte una instancia del modelo en un diccionario que puede ser fácilmente convertido a JSON
+
+"""
+
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
 class Users(db.Model):
-    __tablename__ = "users_i"
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    firstname = db.Column(db.String, unique=False, nullable=False)
-    lastname = db.Column(db.String, unique=False, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    firstname = db.Column(db.String(80), nullable=False)
+    lastname = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email} - {self.username}>'
+        return f'<User {self.username}>'
 
     def serialize(self):
-        return {"id": self.id,
-                "username": self.username,
-                "firstname": self.firstname,
-                "lastname": self.lastname,
-                "email": self.email}
-
-
-class Followers(db.Model):
-    __tablename__ = "followers"
-    id = db.Column(db.Integer, primary_key=True)
-    user_from_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    user_from_to = db.relationship('Users', foreign_keys=[user_from_id])
-    user_to_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    user_to_to = db.relationship('Users', foreign_keys=[user_to_id])
-
-    def __repr__(self):
-        return f'<Followers {self.user_from_id} - {self.user_to_id}>'
-
-    def serialize(self):
-        return {"user_from_id": self.user_from_id,
-                "user_to_id": self.user_to_id}
-
+        return {
+            'id': self.id,
+            'username': self.username,
+            'firstname': self.firstname,
+            'lastname': self.lastname,
+            'email': self.email
+        }
 
 class Posts(db.Model):
-    __tablename__ = "posts"
+    __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    user_to = db.relationship('Users', foreign_keys=[user_id], backref=db.backref('post_to', lazy='select'))
+    user = db.relationship('Users', backref=db.backref('posts', lazy=True))
 
     def __repr__(self):
-        return f'<Posts {self.user_id}>'
+        return f'<Post {self.id}>'
 
     def serialize(self):
-        return {"user_id": self.user_id}
-
+        return {
+            'id': self.id,
+            'user_id': self.user_id
+        }
 
 class Medias(db.Model):
-    __tablename__ = "medias"
+    __tablename__ = 'medias'
     id = db.Column(db.Integer, primary_key=True)
-    medias_type = db.Column(db.String, unique=True, nullable=False)
-    url = db.Column(db.String, unique=True, nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"))
-    post_to = db.relationship('Posts', foreign_keys=[post_id], backref=db.backref('media_to', lazy='select'))
-
+    type = db.Column(db.String(50), nullable=False)
+    url = db.Column(db.String(200), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    post = db.relationship('Posts', backref=db.backref('media', lazy=True))
     def __repr__(self):
-        return f'<Medias {self.medias_type}>'
+        return f'<Media {self.id}, Type: {self.type}>'
 
     def serialize(self):
-        return {"id": self.id,
-                "medias_type": self.medias_type,
-                "url": self.url,
-                "post_id": self.post_id}
+        return {
+            'id': self.id,
+            'type': self.type,
+            'url': self.url,
+            'post_id': self.post_id
+        }
 
+class Followers(db.Model):
+    __tablename__ = 'followers'
+    id = db.Column(db.Integer, primary_key=True)
+    user_from_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_from = db.relationship('Users', foreign_keys=[user_from_id], backref=db.backref('following', lazy='dynamic'))
+    user_to = db.relationship('Users', foreign_keys=[user_to_id], backref=db.backref('followers', lazy='dynamic'))
+
+    def __repr__(self):
+        return f'<Follower {self.user_from_id} follows {self.user_to_id}>'
+
+    def serialize(self):
+        return {
+            'user_from_id': self.user_from_id,
+            'user_to_id': self.user_to_id
+        }
 
 class Comments(db.Model):
-    __tablename__ = "comments"
+    __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
-    comment_text = db.Column(db.String, unique=False, nullable=False)
+    comment_text = db.Column(db.String(500), nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    author_to = db.relationship('Users', foreign_keys=[author_id])
     post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
-    post_to = db.relationship('Posts', foreign_keys=[post_id], backref=db.backref('comment_to', lazy='select'))
+    author = db.relationship('Users', backref=db.backref('comments', lazy=True))
+    post = db.relationship('Posts', backref=db.backref('comments', lazy=True))
 
     def __repr__(self):
-        return f'<Comments {self.comment_text}>'
+        return f'<Comment {self.id}>'
 
     def serialize(self):
-        return {"id": self.id,
-                "comment_text": self.comment_text,
-                "author_id": self.author_id,
-                "post_id": self.post_id}
-
-
-
+        return {
+            'id': self.id,
+            'comment_text': self.comment_text,
+            'author_id': self.author_id,
+            'post_id': self.post_id
+        }
 
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,17 +1,141 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from api.models import db, Users
+from api.models import db, Users, Posts, Medias, Followers, Comments
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
 
 api = Blueprint('api', __name__)
-CORS(api)  # Allow CORS requests to this API
+CORS(api)  
 
-
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route("/login", methods=["POST"])
+def login():
     response_body = {}
-    response_body['message'] = "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    data = request.json
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        response_body['message'] = 'Bad username or password'
+        return response_body, 401
+    access_token = create_access_token(identity=username)
+    response_body['message'] = 'User logged'
+    response_body['access_token'] = access_token
+    # return jsonify(access_token=access_token)
     return response_body, 200
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    response_body = {}
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    response_body['message'] = f'Acceso conceido {current_user}'
+    return response_body, 200
+
+@api.route('/users', methods=['GET', 'POST'])
+def handle_users():
+    response_body = {}
+    if request.method == 'GET':
+        rows = db.session.execute(db.select(Users)).scalars()
+        results = [row.serialize() for row in rows]  # List comprehension
+        response_body['results'] = results
+        response_body['message'] = "Lista de usuarios"
+        return jsonify(response_body), 200
+    if request.method == 'POST':
+        data = request.json
+        username = data.get('username', None)
+        email = data.get('email', None)
+        if not username or not email:
+            response_body['message'] = 'Faltan datos'
+            response_body['results'] = {}
+            return response_body, 400
+        username_exist = db.session.execute(db.select(Users).where(Users.username == username)).scalar()
+        email_exist = db.session.execute(db.select(Users).where(Users.email == email)).scalar()
+        if username_exist or email_exist:
+            response_body['message'] = 'El usuario ya existe'
+            response_body['results'] = {}
+            return response_body, 404
+        row = Users(username = data['username'], 
+                    email = data['email'],
+                    firstname = data['firstname'],
+                    lastname = data['lastname'])
+        db.session.add(row)
+        db.session.commit()
+        response_body['message'] = "Usuario agregado"
+        response_body['results'] = row.serialize()
+        return response_body, 200
+
+@api.route('/users/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_user(user_id):
+    response_body = {}
+    if request.method == 'GET':
+        row = db.session.execute(db.select(Users).where(Users.id == user_id)).scalar()
+        if not row:
+            response_body['results'] = {}
+            response_body['message'] = f'No existe el usuario {user_id}'
+            return response_body, 404
+        response_body['results'] = row.serialize()
+        response_body['message'] = f'recibí el GET request {user_id}'
+        return response_body, 200
+    if request.method == 'PUT':
+        response_body['message'] = f'recibí el PUT request {user_id}'
+        return response_body, 200
+    if request.method == 'DELETE':
+        response_body['message'] = f'recibí el DELETE request {user_id}'
+        return response_body, 200
+
+@api.route('/posts', methods=['GET', 'POST'])
+def handle_posts():
+    response_body = {}
+    if request.method == 'GET':
+        rows = db.session.execute(db.select(Posts)).scalars()
+        results = [row.serialize() for row in rows]  # List comprehension
+        response_body['results'] = results
+        response_body['message'] = "Lista de posts"
+        return jsonify(response_body), 200
+    if request.method == 'POST':
+        data = request.json
+        id = data.get('id', None)
+        user_id = data.get('user_id', None)
+    if not id or not user_id:
+        response_body['message'] = 'Faltan datos'
+        response_body['results'] = {}
+        return response_body, 400
+    id_exist = db.session.execute(db.select(Posts).where(Posts.id == id)).scalar()
+    user_id_exist = db.session.execute(db.select(Posts).where(Posts.user_id == user_id)).scalar()
+    if id_exist or user_id_exist:
+        response_body['message'] = 'El usuario ya existe'
+        response_body['results'] = {}
+        return response_body, 404
+        row = Posts(username = data['id'], 
+                    email = data['user_id'])
+        db.session.add(row)
+        db.session.commit()
+        response_body['message'] = "Post agregado"
+        response_body['results'] = row.serialize()
+        return response_body, 200
+
+
+
+@api.route('/posts/<int:post_id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_post(post_id):
+    response_body = {}
+    if request.method == 'GET':
+        row = db.session.execute(db.select(Posts).where(Posts.id == post_id)).scalar()
+        if not row:
+            response_body['results'] = {}
+            response_body['message'] = f'No existe el usuario {post_id}'
+            return response_body, 404
+        response_body['results'] = row.serialize()
+        response_body['message'] = f'recibí el GET request {post_id}'
+        return response_body, 200
+    if request.method == 'PUT':
+        response_body['message'] = f'recibí el PUT request {post_id}'
+        return response_body, 200
+    if request.method == 'DELETE':
+        response_body['message'] = f'recibí el DELETE request {post_id}'
+        return response_body, 200
+
+
+
